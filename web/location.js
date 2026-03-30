8adbda2f0d5ba7458af1e447dc7f9c9d8d489502
Bart van Stratum <bart@vanstratum.com>
2026-03-30 09:48:22 +0200

    Default or set values to user location if geolocation is shared.

---
+++ web/location.js
@@ -0,0 +1,42 @@
+//
+// Copyright 2026 Wageningen University & Research (WUR)
+// Author: Bart van Stratum
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+(function () {
+    function set_location(lat, lon) {
+        document.getElementById("lat_input").value  = lat;
+        document.getElementById("lon_input").value  = lon;
+        document.getElementById("date_input").value = new Date().toISOString().slice(0, 10);
+    }
+
+    function here_and_now() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (pos) => set_location(
+                    pos.coords.latitude.toFixed(4),
+                    pos.coords.longitude.toFixed(4)
+                ),
+                () => set_location(52, 6)
+            );
+        } else {
+            set_location(52, 6);
+        }
+    }
+
+    document.getElementById("here_and_now_btn").addEventListener("click", here_and_now);
+
+    here_and_now();
+})();
